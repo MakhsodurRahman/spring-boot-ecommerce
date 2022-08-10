7170f1683b07734a66764084b0a473bf4ddc7a4d
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-08-11 02:55:02 +0600

    this class create for  disable http method for product : delete,put,post

---
+++ backend/src/main/java/com/makhsodur/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,31 @@
+package com.makhsodur.ecommerce.config;
+
+import com.makhsodur.ecommerce.entity.Product;
+import com.makhsodur.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+//this class create for  disable http method for product : delete,put,post
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT,HttpMethod.DELETE,HttpMethod.POST};
+
+        //disable http method for product : delete,put,post
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        //disable http method for productCategory : delete,put,post
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+    }
+}
