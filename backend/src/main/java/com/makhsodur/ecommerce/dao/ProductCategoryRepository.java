1f69cfd3f835fdedde55fab3a7dd661acc7f03fb
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-08-14 11:48:59 +0600

    add service

--- backend/src/main/java/com/makhsodur/ecommerce/dao/ProductCategoryRepository.java
+++ backend/src/main/java/com/makhsodur/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,10 @@ package com.makhsodur.ecommerce.dao;
 import com.makhsodur.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+//(collectionResourceRel = "productCategory" = json name )  (path = "product-category" = url path)
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 }
